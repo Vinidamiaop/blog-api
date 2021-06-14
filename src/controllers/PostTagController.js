7e3c3ddb559931dfe161c8a5ec44902c66e92f2c
Vinicius Damião <vinidamiaop@gmail.com>
2021-06-14 15:43:01 -0300

    PostTag Controller refactored

--- src/controllers/PostTagController.js
+++ src/controllers/PostTagController.js
@@ -4,36 +4,84 @@ const prisma = new PrismaClient();
 
 const routes = {
   store: async (req, res) => {
-    if (!req.body.postId || !req.body.tagId) {
-      return res.status(400).json({
-        erros: ["The request should contain postId and tagId."],
-      });
-    }
     try {
-      const tagPost = await prisma.postTag.create({
+      if (!req.body.postId || !req.body.tagId) {
+        return res.status(400).json({
+          erros: ["postId and tagId are required."],
+        });
+      }
+
+      const findTag = await prisma.tag.findUnique({
+        where: { id: req.body.tagId },
+      });
+      const findPost = await prisma.post.findUnique({
+        where: { id: req.body.postId },
+      });
+
+      if (!findTag) {
+        return res.status(404).json({ errors: ["Tag not found."] });
+      }
+
+      if (!findPost) {
+        return res.status(404).json({ errors: ["Post not found."] });
+      }
+
+      const findPostTag = await prisma.postTag.findUnique({
+        where: {
+          postId_tagId: {
+            postId: req.body.postId,
+            tagId: req.body.tagId,
+          },
+        },
+      });
+
+      if (findPostTag) {
+        return res.status(400).json({ errors: ["Relation already existis."] });
+      }
+
+      await prisma.postTag.create({
         data: {
           postId: req.body.postId,
           tagId: req.body.tagId,
         },
       });
 
-      return res.json(tagPost);
+      return res.json({
+        postId: findPost.id,
+        tagId: findTag.id,
+        postTitle: findPost.title,
+        tagTitle: findTag.title,
+      });
     } catch (error) {
-      return res.status(400).json({ errors: [error.message] });
+      return res.status(500).json({
+        errors: ["Unexpected error has occurred. Please, try again."],
+      });
     }
   },
   index: async (req, res) => {
     try {
       const tags = await prisma.postTag.findMany({
         select: {
-          tag: true,
-          post: true,
+          tag: {
+            select: {
+              id: true,
+              title: true,
+            },
+          },
+          post: {
+            select: {
+              id: true,
+              title: true,
+            },
+          },
         },
       });
 
       return res.json(tags);
     } catch (error) {
-      return res.status(400).json({ errors: [error.message] });
+      return res.status(500).json({
+        errors: ["Unexpected error has occurred. Please, try again."],
+      });
     }
   },
   delete: async (req, res) => {
@@ -41,7 +89,7 @@ const routes = {
       if (!req.body.postId || !req.body.tagId) {
         return res
           .status(400)
-          .json({ errors: ["The request should contain postId and tagId."] });
+          .json({ errors: ["postId and tagId are required."] });
       }
 
       const findPostTag = await prisma.postTag.findUnique({
@@ -49,6 +97,20 @@ const routes = {
           postId_tagId: {
             postId: Number(req.body.postId),
             tagId: Number(req.body.tagId),
+          },
+        },
+        select: {
+          post: {
+            select: {
+              id: true,
+              title: true,
+            },
+          },
+          tag: {
+            select: {
+              id: true,
+              title: true,
+            },
           },
         },
       });
@@ -59,7 +121,7 @@ const routes = {
           .json({ errors: ["PostTag relation not found."] });
       }
 
-      const deletedPostTag = await prisma.postTag.delete({
+      await prisma.postTag.delete({
         where: {
           postId_tagId: {
             postId: Number(req.body.postId),
@@ -68,9 +130,11 @@ const routes = {
         },
       });
 
-      return res.json(deletedPostTag);
+      return res.json(findPostTag);
     } catch (error) {
-      return res.status(400).json({ errors: [error.message] });
+      return res.status(500).json({
+        errors: ["Unexpected error has occurred. Please, try again."],
+      });
     }
   },
 };
